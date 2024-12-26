cf5bc3ee39281a8d5d31cb4e1402e018d4c676e9
amarjit <amarjit.yadav@bitsflowtech.com>
2024-12-26 13:05:33 +0530

    base template complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,26 @@
-
+import {
+PasswordInput,
+PasswordStrengthMeter,
+} from "@/components/ui/password-input"
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-          
+      <div>
+      <PasswordInput />
+      
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      <PasswordStrengthMeter />
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
